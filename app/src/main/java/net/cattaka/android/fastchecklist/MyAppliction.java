aeb6e6bdebcd0f56ad16292d7dbda67ac2e8350f
cattaka <cattaka@mail.cattaka.net>
2014-10-27 11:15:40 +0900

    Fix mistake of TelnetSqlite reflection

--- app/src/main/java/net/cattaka/android/fastchecklist/MyAppliction.java
+++ app/src/main/java/net/cattaka/android/fastchecklist/MyAppliction.java
@@ -12,8 +12,8 @@ public class MyAppliction extends Application {
         super.onCreate();
         // SQLiteにアクセするためのサーバーを起動する（開発時専用）
         try {
-            Class<?> clazz = Class.forName("net.cattaka.android.fastchecklist.TelnetSqliteService$TelnetSqliteServer");
-            Thread serverThread = (Thread)clazz.getConstructor(Context.class, int.class).newInstance(this, 12080);
+            Class<?> clazz = Class.forName("net.cattaka.telnetsqlite.TelnetSqliteService");
+            Thread serverThread = (Thread)clazz.getMethod("createTelnetSqliteServer", Context.class, int.class).invoke(null, this, 12080);
             serverThread.start();
         } catch (Exception e) {
             // ignore
