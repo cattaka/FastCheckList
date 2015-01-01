1a6b5ae65899b5aaeb000ec5eb29dc1c1bdc1ad9
cattaka <cattaka@mail.cattaka.net>
2015-01-01 11:48:03 +0900

    Add some tests.

--- app/src/main/java/net/cattaka/android/fastchecklist/core/ContextLogicFactory.java
+++ app/src/main/java/net/cattaka/android/fastchecklist/core/ContextLogicFactory.java
@@ -6,7 +6,7 @@ import android.content.Context;
  * Created by cattaka on 14/11/28.
  */
 public class ContextLogicFactory {
-    private static ContextLogicFactory INSTANCE = new ContextLogicFactory();
+    static ContextLogicFactory INSTANCE = new ContextLogicFactory();
     public ContextLogic newInstance(Context context) {
         return new ContextLogic(context);
     }
